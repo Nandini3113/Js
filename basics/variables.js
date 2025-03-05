34fc6b4005a3f3f8e1cb1f3c8c94192535a47eab
Nandini Nandwani <138558257+Nandini3113@users.noreply.github.com>
2025-03-05 06:03:30 +0000

    variables commit

---
+++ basics/variables.js
@@ -0,0 +1,15 @@
+const accountId = 13456
+let accountEmail= " ttt@gmail.com"
+var accountPwd = "5678"
+accountCity="panipat"
+let accountstate;
+// accountId = 3 // not allowed
+
+accountEmail = "iutytu.com"
+accountPwd = "9876"
+accountCity = "ooty "
+/*
+dont use var to avoid 
+block scope and functional scope
+*/
+console.table([accountId,accountEmail , accountPwd , accountCity , accountstate]);
