12b565bfd363405f324644c2a31668890cf3daba
Nandini Nandwani <138558257+Nandini3113@users.noreply.github.com>
2025-03-08 07:50:18 +0000

    conerse opera

--- basics/conversionOperations.js
+++ basics/conversionOperations.js
@@ -1,10 +1,10 @@
 let score = "33"
 
-console.log(typeof score);
+// console.log(typeof score);
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" =>NaN
@@ -12,13 +12,46 @@ console.log(valueInNumber);
 
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 // 1=>true ; 0=>false
 // "" =>false
 // "ind" =>true
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+// *************Operations *****************
 
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+let str1 = "hii"
+let str2 = " Nan"
+let str3 = str1+str2
+// console.log(str3);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+
+// console.log(+" ");
+
+let num1 , num2 , num3
+ num1 = num2 = num3 = 2+2
+
+ let gameCounter = 104
+ ++gameCounter;
+ console.log(gameCounter);
+ 
+ 
