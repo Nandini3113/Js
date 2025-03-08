70a686d2680b1b0f0d604654a52c1a50c4a58f8f
Nandini Nandwani <138558257+Nandini3113@users.noreply.github.com>
2025-03-08 14:09:29 +0000

    numm

---
+++ basics/nums_and_maths.js
@@ -0,0 +1,13 @@
+const score = 400
+const balance = new Number (100)
+console.log(score);
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber = 2334.9865
+console.log(otherNumber.toPrecision(5));
+
+
+
