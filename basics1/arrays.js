d45906092965cbb86d1344c257c6e9e12b2ce067
Nandini Nandwani <138558257+Nandini3113@users.noreply.github.com>
2025-03-09 09:37:06 +0000

    arr

---
+++ basics1/arrays.js
@@ -0,0 +1,37 @@
+const myArr =[0,1,3,4,5,6]
+
+const myAArr = new Array (1,2,3 ,4 )
+console.log(myArr[4]);
+
+// Array Methods 
+
+// myArr.push(7)
+// myArr.push(7)
+// myArr.pop
+// myArr.unshift(9)
+
+// myArr.shift()
+// 
+// const newArr = myArr.join()
+// console.log(typeof newArr);
+// console.log(typeof myArr);
+
+
+
+// console.log(myArr);
+
+
+
+
+
+// Slice , splice
+console.log("A" , myArr);
+
+const myn1 = myArr.slice(1,3)
+console.log(myn1);
+console.log("B" , myArr);
+
+const myn2 = myArr.splice(1,3)
+console.log(myn2);
+
+console.log("C" , myArr);
